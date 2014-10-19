0f5e1058ac0ed83ddaaf6364014ec066096638a7
CliveLi <cliveli@163.com>
2014-10-19 10:39:27 +0800

    add ng-bind data to view 2

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,7 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope',function($scope) {
+ $scope.message_number = 5;
 
 }]);
